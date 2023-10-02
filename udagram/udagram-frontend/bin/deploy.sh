8e9ee3264e9ca1d443d5c068349870065f104749
trany3004 <69740721+trany3004@users.noreply.github.com>
2023-10-03 02:26:05 +0700

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://yttn2-udagram/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://yttn2-udagram/
+aws s3 cp --recursive --acl public-read ./www s3://yttn2-udagram-final/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://yttn2-udagram-final/
